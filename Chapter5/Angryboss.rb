18fb21d00513e6e0e691c96b2aab8ed1a53eb573
Puja Sharma <pujashrma151195@gmail.com>
2024-05-13 11:39:50 +0530

    hello

---
+++ Chapter5/Angryboss.rb
@@ -0,0 +1,20 @@
+class Angryboss
+	def angry_boss(names = String[])
+
+		puts "wHAT DO YOU WANT?! MAKE IT FAST, #{names[rand(2)].upcase}!"
+        request = gets.chomp
+        puts "WHAT DO YOU MEAN \"#{request.upcase}\" ?!? YOU'RE FIRED!!"
+
+    end
+end
+
+obj = Angryboss.new
+obj.angry_boss(["sam" , "john" , "charles" , "nick"])
+obj.angry_boss(["harry" , "jay" , "chris" , "veer"])
+
+
+
+# names = ["sam" , "john" , "charles" , "nick"]
+# puts "wHAT DO YOU WANT?! MAKE IT FAST, #{names[rand(2)].upcase}!"
+# request = gets.chomp
+# puts "WHAT DO YOU MEAN \"#{request.upcase}\" ?!? YOU'RE FIRED!!"
