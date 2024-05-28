0986782d2010e1c85057d5c00844e453657274ac
Puja Sharma <pujashrma151195@gmail.com>
2024-05-28 17:53:35 +0530

    Account activation started

--- Twitter/test/integration/users_login_test.rb
+++ Twitter/test/integration/users_login_test.rb
@@ -6,24 +6,18 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     @user = users(:michael)
   end
 
-  test "login with valid information followed by logout"  do
+  test "login with valid information"  do
     get login_path
     post login_path, params: { session: { email: @user.email, password: 'password'} }
-    assert is_logged_in?
     assert_redirected_to @user
     follow_redirect!
     assert_template 'users/show'
     assert_select "a[href=?]", login_path, count: 0
     assert_select "a[href=?]", logout_path
     assert_select "a[href=?]", user_path(@user)
-    delete logout_path
-    assert_not is_logged_in?
-    assert_redirected_to root_url
-    follow_redirect!
-    assert_select "a[href=?]", login_path
-    assert_select "a[href=?]", logout_path, count: 0
-    assert_select "a[href=?]", user_path(@user), count: 0
   end
+
+  
 
   test "login with valid email/invalid password" do
     get login_path
@@ -48,8 +42,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
 
   test "login with remembering" do
     log_in_as(@user, remember_me: '1')
-    assert_equal cookies['remember_token'], 
-    assigns(@user).remember_token
+    assert_equal cookies['remember_token'], assigns(:user).remember_token
   end
 
   test "login without remembering" do
@@ -58,6 +51,25 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
    # Log in again and verify that the cookie is deleted.
     log_in_as(@user, remember_me: '0')
     assert_empty cookies['remember_token']
+  end
+
+  test "login with valid information followed by logout"  do
+    get login_path
+    post login_path, params: { session: { email: @user.email, password: 'password'} }
+    assert is_logged_in?
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
+    delete logout_path
+    assert_not is_logged_in?
+    assert_redirected_to root_url
+    follow_redirect!
+    assert_select "a[href=?]", login_path
+    assert_select "a[href=?]", logout_path, count: 0
+    assert_select "a[href=?]", user_path(@user), count: 0
   end
 
 end
