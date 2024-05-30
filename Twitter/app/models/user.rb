77afcc59f98b678daa436a49860d8b1e672a3c19
Puja Sharma <pujashrma151195@gmail.com>
2024-05-30 17:48:10 +0530

    Password reset and Micropost model

--- Twitter/app/models/user.rb
+++ Twitter/app/models/user.rb
@@ -60,6 +60,11 @@ class User < ApplicationRecord
    def send_password_reset_email
     UserMailer.password_reset(self).deliver_now
    end
+   
+   # Returns true if a password reset ha expired.
+   def password_reset_expired?
+    reset_sent_at < 2.hours.ago 
+  end
 
 
    private
