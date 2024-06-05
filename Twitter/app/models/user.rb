c251a8c80b3ec123c59dea3bdd874e0caf17c009
Puja Sharma <pujashrma151195@gmail.com>
2024-06-05 16:21:17 +0530

    Sample App completed

--- Twitter/app/models/user.rb
+++ Twitter/app/models/user.rb
@@ -78,7 +78,8 @@ class User < ApplicationRecord
    # Defines a proto-feed.
    # See "Following users" for the full implementation.
    def feed
-    Micropost.where("user_id = ?", id)
+    part_of_feed = "relationships.follower_id = :id or microposts.user_id = :id"
+    Micropost.joins(user: :followers).where(part_of_feed, { id: id })
    end
 
    # Follows a user.
